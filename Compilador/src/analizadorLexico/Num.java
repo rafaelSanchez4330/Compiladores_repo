691258851be781acf047339184bfbb260c71f8c4
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-26 13:51:39 -0600

    Add files via upload

---
+++ Compilador/src/analizadorLexico/Num.java
@@ -0,0 +1,15 @@
+
+package analizadorLexico;
+
+public class Num extends Token {
+    public final int valor;
+
+    public Num(int v) {
+        super(Etiqueta.NUM);
+        valor = v;
+    }
+
+    public String toString() {
+        return "" + valor;
+    }
+}
